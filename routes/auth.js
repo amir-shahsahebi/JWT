a79a5cbdae65e2752c4d1e4cf563deda3a3d2631
amir shahsahebi <shahsahebiamir@gmail.com>
2022-06-08 15:41:43 +0430

    validaion email & password done

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,28 @@
 const router = require("express").Router();
+const { check, validationResult } = require("express-validator");
+// in this line: => router.post("/singup",[], (req, res) => {}  // => [] is middleware
+router.post(
+  "/singup",
+  [
+    check("email", "Please provide a valid email").isEmail(),
+    check(
+      "password",
+      "Please provide a password that is greater than 5 characters"
+    ).isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const { password, email } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        error: errors.array(),
+      });
+    }
 
-router.post("/singup", (req, res) => {
-  const { password, email } = req.body;
-  console.log(password, email);
-  res.send("auth rout working");
-});
+    res.send("Validation Pass");
+  }
+);
 
 module.exports = router;
