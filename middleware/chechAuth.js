1ce7d2614571bddd2efdfc1be278ba9456f84fc7
amir shahsahebi <shahsahebiamir@gmail.com>
2022-06-09 23:45:44 +0430

    middleware updated

--- middleware/chechAuth.js
+++ middleware/chechAuth.js
@@ -1,12 +1,26 @@
+const JWT = require("jsonwebtoken");
+
 module.exports = async (req, res, next) => {
-  let userValid = true;
-  if (userValid) {
-    next();
-  } else {
+  const token = req.header("x-auth-token");
+
+  if (!token) {
     return res.status(400).json({
       error: [
         {
-          msg: "Access denied",
+          msg: "no token found",
+        },
+      ],
+    });
+  }
+  try {
+    let user = await JWT.verify(token, "allsaflsafa55656565");
+    req.user = user.email;
+    next();
+  } catch (error) {
+    return res.status(400).json({
+      error: [
+        {
+          msg: "token invalid",
         },
       ],
     });
