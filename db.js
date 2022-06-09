406831304c14888b663d2138badeb63d33e0c35e
amir shahsahebi <shahsahebiamir@gmail.com>
2022-06-09 20:46:01 +0430

    hashing paaword completed

---
+++ db.js
@@ -0,0 +1,10 @@
+const users = [
+  {
+    email: "amir@gmail.com",
+    password: "123456789",
+  },
+];
+
+module.exports = {
+  users,
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const { check, validationResult } = require("express-validator");
+const { users } = require("../db");
+const bcrypt = require("bcrypt");
 // in this line: => router.post("/singup",[], (req, res) => {}  // => [] is middleware
 router.post(
   "/singup",
@@ -12,17 +14,44 @@ router.post(
       min: 6,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const { password, email } = req.body;
+
+    // validated the input
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({
         error: errors.array(),
       });
     }
+    let user = users.find((user) => {
+      return user.email === email;
+    });
+    if (user) {
+      res.status(400).json({
+        error: [
+          {
+            msg: "This user already exist",
+          },
+        ],
+      });
+    }
 
+    let hashedPassword = await bcrypt.hash(password, 10);
+
+    users.push({
+      email,
+      password: hashedPassword,
+    });
+
+    console.log(hashedPassword);
+    // validate if user doesn't already exist
     res.send("Validation Pass");
   }
 );
+
+router.get("/all", (req, res) => {
+  res.json(users);
+});
 
 module.exports = router;
